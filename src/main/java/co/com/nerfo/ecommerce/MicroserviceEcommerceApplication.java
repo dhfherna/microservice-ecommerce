3044a78860311d8ec936175e73e7fa4d54949f25
dhfherna <dhfherna@bancolombia.com.co>
2024-12-16 10:41:41 -0500

    Microservice base

---
+++ src/main/java/co/com/nerfo/ecommerce/MicroserviceEcommerceApplication.java
@@ -0,0 +1,13 @@
+package co.com.nerfo.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/nerfo/ecommerce/MicroserviceEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.nerfo.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
